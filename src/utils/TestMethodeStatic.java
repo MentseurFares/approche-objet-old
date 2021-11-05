025d710e96161f2ea7773831aab6d7bb0ba4b8d9
Mentseur Fares <mentseur.fares@hotmail.fr>
2021-11-05 16:54:45 +0100

    approche-object TP4 (Utilisation des methodes existantes dans les librairies - exemple de java.lang.Integer

---
+++ src/utils/TestMethodeStatic.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class TestMethodeStatic {
+
+	public static void main(String[] args) {
+
+		String chaine = "12";
+		int value =Integer.parseInt(chaine);
+		System.out.println(value);
+		int a = 15, b=45;
+		int max;
+		max = java.lang.Integer.max(a, b);
+		System.out.println("le maximum entre "+a+" et "+b+" : "+max);
+	}
+}
