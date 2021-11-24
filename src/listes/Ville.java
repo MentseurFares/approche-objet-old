947dd4a91644df0a5e99a12a6728ca8f3d761fc3
Mentseur Fares <mentseur.fares@hotmail.fr>
2021-11-24 11:40:01 +0100

    TP - Recensement TP - Test Exception

--- src/listes/Ville.java
+++ src/listes/Ville.java
@@ -1,13 +1,25 @@
 package listes;
 
-public class Ville implements Comparable<Ville>{
+import java.util.Objects;
+
+import fr.diginamic.testenumeration.Continent;
+
+public class Ville implements Comparable<Ville> {
 	private String name;
 	private int nbHabitants;
+	private Continent continent;
 
 	public Ville(String name, int nbHabitants) {
 		super();
 		this.name = name;
 		this.nbHabitants = nbHabitants;
+	}
+
+	public Ville(String name, int nbHabitants, Continent continent) {
+		super();
+		this.name = name;
+		this.nbHabitants = nbHabitants;
+		this.continent = continent;
 	}
 
 	public String getName() {
@@ -25,20 +37,45 @@ public class Ville implements Comparable<Ville>{
 	public void setNbHabitants(int nbHabitants) {
 		this.nbHabitants = nbHabitants;
 	}
-	
+
 	@Override
-	public String toString() {		
-		return this.getName() + " a "+this.getNbHabitants() + " habitants" ;
+	public String toString() {
+		if (continent != null) {
+			return this.getName() + " du continent d'" + this.continent.getLibelle() + " a " + this.getNbHabitants() + " habitants";
+		}
+		return this.getName() + " a " + this.getNbHabitants() + " habitants";
 	}
 
 //	@Override
 //	public int compareTo(Ville autre) {		
 //		return this.name.compareTo(autre.getName());		
 //	}
-	
+
 	@Override
 	public int compareTo(Ville autre) {
 		return Integer.compare(nbHabitants, autre.nbHabitants);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, nbHabitants);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Ville)) {
+			return false;
+		}
+		Ville other = (Ville) obj;
+		return name.equals(other.getName()) && nbHabitants == other.getNbHabitants();
+	}
+
+	public Continent getContinent() {
+		return continent;
+	}
+
+	public void setContinent(Continent continent) {
+		this.continent = continent;
 	}
 
 }
